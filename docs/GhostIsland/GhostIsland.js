a07b7d32397c4cebd49cd28a8fe2700e4b203c64
eebinger <ethanebinger@gmail.com>
2018-05-15 01:22:55 -0400

    vote 12 being duplicated and called as vote 13

--- docs/GhostIsland/GhostIsland.js
+++ docs/GhostIsland/GhostIsland.js
@@ -760,7 +760,9 @@ function calculateScores(scores, results, responses, calcType) {
                     if (results[i].vote === cur_vote) {
                         var val_vote = 'Vote ' + String(results[i].vote);
                         // Determine by team if before merge but no swap:
-                        if (inArray(cur_player+"_"+String(cur_vote),name_ep_count) || cur_vote ===13) {
+                        if (cur_vote === 13 && inArray(cur_player+"_"+String(cur_vote),name_ep_count) === false) {
+                            console.log("skipping accidental duplicate of vote 12: "+cur_player);
+                        } else if (inArray(cur_player+"_"+String(cur_vote),name_ep_count)) {
                             console.log("duplicate: "+cur_player+"_"+String(cur_vote));
                         } else if (results[i].merge === 'Yes' /*|| results[i].merge === 'Swap'*/) {
                             if (results[i].vote < 13) {
